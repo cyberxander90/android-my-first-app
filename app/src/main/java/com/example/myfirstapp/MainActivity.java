cf5f53d730a1923f778114b9b23cec2c49402bdf
inesaintmartin <ines.saint@mercadolibre.com>
2020-01-14 11:05:25 -0300

    Improve first activity

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    /** Called when the activity is first created. */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView firstTextView = findViewById(R.id.first_text);
+        firstTextView.setText("Primer mensaje de la app");
+
+        Button firstButton = findViewById(R.id.first_button);
+        firstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Click", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+
+
 }
